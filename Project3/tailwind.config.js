eee2d3b17fe2c0364f0f9e42d33c37fde575a9db
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-30 09:58:07 +0530

    few changes made

--- Project3/tailwind.config.js
+++ Project3/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        urbanist: ['"Urbanist"', "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
